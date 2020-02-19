aa38ee8ad3b65bb69ed23324d722920e130b55be
Arslan Khalid <arslankhalid067@gmail.com>
2020-02-19 18:41:00 +0500

    Practice Callbacks

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 class Article < ApplicationRecord
     validates :title,:body, presence: true
-    validates :title, uniqueness: true, length: {minimum: 2}
+    validates :title, uniqueness: true, length: {minimum: 2, maximum: 10}
+
+    before_create do
+        self.title="Callback appends " +self.title
+    end
+    after_initialize do |article|
+        puts "You have initialized article with id #{article.id}"
+    end
+    after_find do |article|
+        puts "You have found article with id #{article.id}"
+    end
+    after_touch do |article|
+        puts "You have touch Article"
+    end
 end
