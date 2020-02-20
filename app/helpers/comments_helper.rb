bfbfaea58a09af6c4629742e93de6a05b28a3052
Arslan Khalid <arslankhalid067@gmail.com>
2020-02-20 19:39:34 +0500

    Added Helper method

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,9 +1,2 @@
 module CommentsHelper
-    def has_comments(article)
-        if(article.comments.count>0)
-            return true
-        else
-            return false
-        end
-    end
 end
